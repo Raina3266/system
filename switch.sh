0b0f327217b89813564966b590d97c9b125b478c
Raina <raina@kaleidoscope.com>
2025-09-21 13:05:19 +0100

    remove old home-manager

--- switch.sh
+++ switch.sh
@@ -1,8 +1,13 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 set -euxo pipefail
 
 git add -A
-sudo nixos-rebuild switch --flake .#thinkpad --option eval-cache false --show-trace
+
+if test -f /etc/NIXOS; then
+    sudo nixos-rebuild switch --flake .#thinkpad --option eval-cache false --show-trace
+else
+    nix run home-manager/master -- switch --flake .
+fi
 
 # useful for debugging the above command
 # --option eval-cache false --show-trace
