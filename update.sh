6d75ba22d4eafb8ed2869f76a00d8080b64c0dac
Raina <cgl0326@outlook.com>
2025-10-19 03:35:47 +0100

    update

--- update.sh
+++ update.sh
@@ -1,12 +1,4 @@
 #! /usr/bin/env sh
 set -euxo pipefail
 
-git add -A
-
-if test -f /etc/NIXOS; then
-    sudo nix flake update
-    ./switch.sh
-else
-    nix flake update
-    ./switch.sh
-fi
+sudo nix flake update
