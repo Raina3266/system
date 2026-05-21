450070e7d61d51ffe8e2704bf1a0bc12f139aa1a
Raina <cgl0326@outlook.com>
2026-05-21 15:11:57 +0100

    update

--- home/ocr.nix
+++ home/ocr.nix
@@ -3,16 +3,16 @@ let
   ocrScreenshot = pkgs.writeShellScriptBin "ocr-screenshot.sh" ''
     IMG_PATH=/tmp/screenshot.png
 
+    # Take screenshot
     gnome-screenshot --area --file=$IMG_PATH
 
+    # Run OCR
     tesseract $IMG_PATH /tmp/ocr-output
 
+    # Copy to Wayland clipboard and notify
     TEXT=$(cat /tmp/ocr-output.txt)
-    echo $TEXT | wl-copy
+    echo "$TEXT" | wl-copy
     notify-send "Copied: $TEXT"
-
-    # pkgs.translate-shell
-    # trans -t zh-CN hello
   '';
 in
 {
@@ -22,4 +22,29 @@ in
     wl-clipboard
     ocrScreenshot
   ];
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      enable-hot-corners = false;
+    };
+
+    "org/gnome/desktop/interface" = {
+      show-battery-percentage = true;
+    };
+
+    "org/gnome/mutter" = {
+      center-new-windows = true;
+    };
+
+    # Custom keybindings
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ocr-shortcut/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ocr-shortcut" = {
+      binding = "<Alt>s";
+      command = "${ocrScreenshot}/bin/ocr-screenshot.sh";
+      name = "OCR Screenshot";
+    };
+  };
 }
--- home/personal/default.nix
+++ home/personal/default.nix
@@ -19,26 +19,9 @@
       shotcut
       qbittorrent
       android-tools
+      immich
       jellyfin
       jellyfin-web
     ];
-    dconf.settings = {
-      "org/gnome/desktop/interface" = {
-        enable-hot-corners = false;
-      };
-
-      "org/gnome/desktop/interface" = {
-        show-battery-percentage = true;
-      };
-
-      "org/gnome/mutter" = {
-        center-new-windows = true;
-      };
-
-      # Custom keybindings
-      "org/gnome/desktop/wm/keybindings" = {
-        close = [ "<Super>q" ];
-      };
-    };
   };
 }
