62be7e407e774a73119dda331912fea33f9a5317
raina <cgl0326@outlook.com>
2026-08-13 21:15:17 +0100

    small clean up

--- home/niri/waybar/top-center.nix
+++ home/niri/waybar/top-center.nix
@@ -39,13 +39,7 @@ in
     "custom/lyrics" = {
       hide-empty-text = true;
       return-type = "json";
-      format = "{icon} {0}";
-      format-icons = {
-        playing = "󰝚 ";
-        paused = "󰝚 ";
-        lyric = "";
-        music = "󰝚 ";
-      };
+      format = "󰝚 {text}";
       exec-if = "pgrep -x tauon >/dev/null || pgrep -x kid3 >/dev/null";
       exec = "${pkgs.waybar-lyric}/bin/waybar-lyric -qfpartial";
       on-click = "${pkgs.waybar-lyric}/bin/waybar-lyric play-pause";
