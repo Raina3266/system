af57b12ba628963cdcbcfeec107550a09d26f5a3
raina <cgl0326@outlook.com>
2026-08-31 19:20:15 +0100

    delete patch

--- home/default.nix
+++ home/default.nix
@@ -12,10 +12,6 @@ let
     rm -f $out/share/icons/hicolor/scalable/apps/com.github.qarmin.czkawka-symbolic.svg
     rm -f $out/share/metainfo/com.github.qarmin.czkawka.metainfo.xml
   '';
-
-  dolphinWithoutHistoryMenuIndicators = pkgs.kdePackages.dolphin.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [ ./dolphin-hide-history-menu-indicators.patch ];
-  });
 in
 {
   imports = [
@@ -64,7 +60,7 @@ in
 
     # Qt/Kde based
     kdePackages.elisa
-    dolphinWithoutHistoryMenuIndicators
+    kdePackages.dolphin
     kdePackages.ark
     kdePackages.baloo
     kdePackages.baloo-widgets
